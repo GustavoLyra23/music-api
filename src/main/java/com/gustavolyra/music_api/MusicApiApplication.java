b6a84cc2622b7352e429e5f148b6d77c69998b8e
GustavoLyra23 <gustavolyra23@gmail.com>
2024-08-29 16:47:42 -0300

    first commit

---
+++ src/main/java/com/gustavolyra/music_api/MusicApiApplication.java
@@ -0,0 +1,13 @@
+package com.gustavolyra.music_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavolyra/music_api/MusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavolyra.music_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
